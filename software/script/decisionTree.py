c636d52d4500f47309e5b7163f96b85b50ac3583
huigang39 <huigang39@outlook.com>
2022-03-11 11:39:59 +0800

    完成了特征值提取

---
+++ software/script/decisionTree.py
@@ -0,0 +1,10 @@
+from extractEigen import *
+from sklearn.tree import DecisionTreeClassifier
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ software/script/drawGraph.py
@@ -0,0 +1,20 @@
+from extractEigen import *
+import matplotlib.pyplot as plt
+
+
+class Graph:
+    def __init__(self, data: Eigen) -> None:
+        self.data = data
+
+    def drawLine(self) -> None:
+        """
+        绘制线图
+        """
+        plt.plot(self.data.getDataDiff())
+        plt.show()
+
+
+if __name__ == '__main__':
+    test = Graph(Eigen('BVH/drink_water/normal.bvh',
+                 'RightShoulder', ['x', 'y', 'z']))
+    test.drawLine()
