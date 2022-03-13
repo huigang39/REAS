b2cd5a04b971c261cf3e26da0c0af8d47484d524
huigang39 <huigang39@outlook.com>
2022-03-13 19:04:08 +0800

    添加DTW算法，但还没成功

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/huigang/codespace/Rehabilitation_Assessment/software
-# Build directory: /home/huigang/codespace/Rehabilitation_Assessment/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
